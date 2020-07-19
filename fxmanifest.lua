e54753103d8ac51413bdc068cbe4cfdd68e24e1e
Fredrik Vold <fredrik@webkonsept.com>
2020-07-19 22:28:38 +0200

    It's not going to work without a manifest!

---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'bodacious'
+game 'gta5'
+
+client_scripts { 'cl_entitydata.lua', 'entitydata.lua' }
+server_scripts { 'sv_entitydata.lua' }
+
+description 'Arbituary entity data experiment'
+author 'Demonen'
