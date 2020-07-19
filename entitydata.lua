b7048ffc610ce61534211122b59f3fb70d19b991
Fredrik Vold <fredrik@webkonsept.com>
2020-07-19 22:08:01 +0200

    UNTESTED, but ready to test, I guess

--- entitydata.lua
+++ entitydata.lua
@@ -1,35 +1,84 @@
-local ID_SUBSCRIBERS = {}
+local resource = GetCurrentResourceName()
+if GetCurrentResourceName() ~= 'entitydata' then
+    function EntityGetDataByNetworkId(...)
+        return exports.entitydata:EntityGetDataByNetworkId(...)
+    end
+    function EntityGetData(...)
+        return exports.entitydata:EntityGetData(...)
+    end
+end
+
+local PENDING = {}
+local SUBSCRIBERS = {}
 RegisterNetEvent('entitydata:set')
 AddEventHandler('entitydata:set',function(netId, key, value)
-    if ID_SUBSCRIBERS[netId] then
-        if NetworkDoesEntityExistWithNetworkId(netId) then
-            local entity = NetworkGetEntityFromNetworkId(netId)
-            for name, sub in pairs(ID_SUBSCRIBERS[netId]) do
-                if sub.key == key then
-                    local success, message = pcall(sub.callback, entity, key, value)
-                    if not success then
-                        print('ERROR in '..name..': '..message)
-                    end
-                end
+    if SUBSCRIBERS[key] then
+        if not PENDING[netId] then
+            PENDING[netId] = {}
+        end
+        PENDING[netId][key] = value
+    end
+end
+
+function TriggerSubscibers(entity, key, value)
+    if SUBSCRIBERS[key] then
+        local subscriberCount = 0
+        for subscriber, callback in pairs(SUBSCRIBERS[key]) do
+            local success, message = pcall(callback, entity, key, value)
+            if success then
+                subscriberCount = subscriberCount + 1
+            else
+                print('ENTITY DATA SUBSCRIBER ERROR: '..message..' in '..subscriber)
+                SUBSCRIBERS[key][subscriber] = nil
             end
         end
+        if subscriberCount == 0 then
+            SUBSCRIBERS[key] = nil
+        end
     end
-    if KEY_SUBSCRIBERS[key] then
+end
+
+Citizen.CreateThread(function()
+    while true do
+        for netId, data in pairs(PENDING) do
+            if NetworkDoesEntityExistWithNetworkId(netId) then
+                local entity = NetworkGetEntityFromNetworkId(netId)
+                for key, value in pairs(data) do
+                    TriggerSubscibers(entity, key, value)
+                end
+                PENDING[netId] = nil
+            end
+            Citizen.Wait(0)
+        end
+        Citizen.Wait(0)
+    end
 end)
 
-function SubscribeNetEntityData(netId, key, callback)
-
-    assert(typeof(netId) == 'number' and key % 1 == 0 and key > 0, 'Argument 1, netId, must be a positive integer for SubscribeNetEntityData')
+function SubscribeToEntityData(key, callback)
     assert(typeof(key) == 'string' and key ~= '', 'Argument 2, key, must be a string, and can not be empty for SubscribeNetEntityData')
     assert(typeof(callback) == 'function', 'Argument 3, callback function, is required for SubscribeNetEntityData')
 
     local cbInfo = debug.getinfo(callback, "S")
     local subscriber = string.format('%s:%s', cbInfo.short_src, cbInfo.linedefined)
+    if not SUBSCRIBERS[key] then
+        SUBSCRIBERS[key] = {}
+    end
+    SUBSCRIBERS[key][subscriber] = callback
+    return subscriber
+end
 
-    if ID_SUBSCRIBERS[netId] then
-        ID_SUBSCRIBERS[subscriber] = {
-            key = key,
-            callback = callback,
-        }
+function UnsubscribeFromEntityData(subscriber)
+    for key, subscribers in pairs(SUBSCRIBERS) do
+        local subscriberCount = 0
+        for candidate, callback in pairs(subscribers) do
+            if candidate == subscriber then
+                subscribers[candidate] = nil
+            else
+                subscriberCount = subscriberCount + 1
+            end
+        end
+        if subscriberCount == 0 then
+            SUBSCRIBERS[key] = nil
+        end
     end
 end
